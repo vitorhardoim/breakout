8b09aefba914b7afd69e5ab97f9e0651a1a18a1f
lffloyd <lfmatosmelo@id.uff.br>
2019-03-05 12:49:52 -0300

    Item manipulation methods tested using List instead of Array (with problems; reverted to array for this version).

--- Breakout/Assets/Scripts/Player/Inventory.cs
+++ Breakout/Assets/Scripts/Player/Inventory.cs
@@ -6,7 +6,10 @@ public class Inventory : MonoBehaviour {
     public int qtdItems;
     GameObject[] activeItems = new GameObject[9];
     List<Vector2> itemsPositions = new List<Vector2>();
+    
     public bool[] usedSlots = new bool[9];
+
+    //public List<GameObject> pups = new List<GameObject>();
 
 
     public void Add(GameObject it) {
@@ -18,13 +21,24 @@ public class Inventory : MonoBehaviour {
             usedSlots[qtdItems] = true;
             qtdItems++;
         }
-        
     }
+
+    /*public void Add(GameObject it) {
+        it.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        if (ItemIsInList(it)) Destroy(it);
+        else {
+        	if (pups.Count < 9) {
+        		pups.Add(it);
+        		it.transform.position = itemsPositions[qtdItems];
+        		qtdItems++;
+        	}
+        }
+    }*/
 
     public void RedefinePosition(GameObject it) {
         int position = System.Array.IndexOf(activeItems, it);
         for (int i = 0; i < position; i++) {
-            if(usedSlots[i] == false) {
+            if (usedSlots[i] == false) {
                 it.transform.position = itemsPositions[i];
                 usedSlots[i] = true;
                 usedSlots[position] = false;
@@ -32,6 +46,15 @@ public class Inventory : MonoBehaviour {
             }
         }
     }
+
+    /*public void RedefinePosition(GameObject it) {
+        for (int i = 0; i < qtdItems; i++) {
+            if (pups[i].tag == it.tag) {
+				pups[i].transform.position = itemsPositions[i];
+				return;
+            }
+        }
+    }*/
 
     public bool ItemIsInList(GameObject it) {
         foreach (GameObject itm in activeItems) {
@@ -46,6 +69,19 @@ public class Inventory : MonoBehaviour {
         return false;
     }
 
+    /*public bool ItemIsInList(GameObject it) {
+        foreach (GameObject itm in pups) {
+            if (itm != null) {
+                if (itm.tag == it.tag) {
+                    if (it.tag == "SpeedUp") itm.GetComponent<SpeedPUp>().RefillTime();
+                    if (it.name == "TazerUp") itm.GetComponent<TazerPUp>().RefillTime();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }*/
+
     public void Remove(GameObject it) {
         foreach (GameObject itm in activeItems) {
             if (itm != null) {
@@ -53,10 +89,20 @@ public class Inventory : MonoBehaviour {
                     int temp = System.Array.IndexOf(activeItems, itm);
                     activeItems[temp] = null;
                     usedSlots[temp] = false;
+                    qtdItems--;
                 }
             }
         }
     }
+
+    /*public void Remove(GameObject it) {
+        foreach (GameObject itm in pups) {
+            if (itm.tag == it.tag) {
+            	pups.Remove(itm);
+            	qtdItems--;
+            }
+        }
+    }*/
 
     public void FillPositions() { for (int i = 0; i < 9; i++) itemsPositions.Add(new Vector2(8.33f, 4.22f - (0.7f*i))); }
 }
